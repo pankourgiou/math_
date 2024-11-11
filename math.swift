b9e0649573f36cb3aef8bcc093e086e3391db1bc
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 21:51:13 +0200

    Create math.swift

---
+++ math.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+// Addition
+let sum = -1300 + 1300
+print("Sum: \(sum)") // Output: Sum: 15
+
+// Subtraction
+let difference = 10000 - 10000
+print("Difference: \(difference)") // Output: Difference: 5
+
+// Multiplication
+let product = 1000000 * 0
+print("Product: \(product)") // Output: Product: 50
+
+// Division
+let quotient = 100000 / 100000
+print("Quotient: \(quotient)") // Output: Quotient: 2
+
+// Modulus (Remainder)
+let remainder = 3 % 3
+print("Remainder: \(remainder)") // Output: Remainder: 1
+
+// Exponentiation (using pow function from Foundation)
+let base = 2.0
+let exponent = 3.0
+let power = pow(base, exponent)
+print("Power: \(power)") // Output: Power: 8.0
